c795d391f7e31f81674f17e8adeefda80277b71d
Misho Krastev <misho.kr+github@gmail.com>
2016-10-18 15:07:57 -0700

    defined cpp package group for OpenMPI dev

--- filter_plugins/plugins.py
+++ filter_plugins/plugins.py
@@ -1,5 +1,7 @@
 # -*- Mode: Python -*-
 
+import string
+
 from ansible import errors
 
 def make_ec2_tag_filters(*vm_tags):
@@ -14,6 +16,10 @@ def make_ec2_tag_filters(*vm_tags):
     # return { x:x for x in range(10) }
     return { "tag:"+k:v for k,v in all_tags }
 
+def string_2_list(s):
+    """The ommission of 'strings.split' from the Jinja filters is baffling"""
+
+    return([string.strip(s2) for s2 in string.split(str(s), ',')])
 
 class FilterModule(object):
     '''Ansible helper jinja2 filters'''
@@ -21,4 +27,5 @@ class FilterModule(object):
     def filters(self):
         return {
             'vm_tags_2_aws_filters':    make_ec2_tag_filters,
+            'str2list':                 string_2_list,
         }
